69802673e6d02b8492ca8ed240ec95104ef85275
Witton666 <zhangweitong6@gmail.com>
2021-09-10 21:05:53 +0800

    version 0: original

---
+++ PokerProject/src/poker/Poker.java
@@ -0,0 +1,75 @@
+package poker;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Poker.
+ */
+public class Poker {
+	
+	/**
+	 * Test find bugs.
+	 */
+	public void testFindBugs() {
+		String a=null;
+		if(a.equals("")) return;
+	}
+
+	
+	/**
+	 * Checks if is full house.
+	 *
+	 * @param cards the cards
+	 * @param n the n
+	 * @return true, if is full house
+	 */
+	public boolean isFullHouse(String cards[], int n) {
+		return isThreeOfaKind(cards, n) && isTwoPairs(cards, n);
+	}
+	
+	/**
+	 * Checks if is three ofa kind.
+	 *
+	 * @param cards the cards
+	 * @param n the n
+	 * @return true, if is three ofa kind
+	 */
+	public boolean isThreeOfaKind(String cards[], int n) {
+		for (int i=0; i<n-2; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1) && 
+					cards[i+1].charAt(1) == cards[i+2].charAt(1))
+					return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks if is two pairs.
+	 *
+	 * @param cards the cards
+	 * @param n the n
+	 * @return true, if is two pairs
+	 */
+	public boolean isTwoPairs(String cards[], int n) {
+		int count = 0;
+		for (int i=0; i<n-1; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
+				count++;
+				i++;
+			}
+		}
+		if (count == 2)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String args[]) {
+		System.out.println(new Poker().isFullHouse(
+				new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
+	}
+}
